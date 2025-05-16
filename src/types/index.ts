5f7e69f5b4efc5da60e35dd198494addad57977a
Tareqitos <contact@tareqitos.com>
2025-05-17 01:09:37 +0200

    Style update

--- src/types/index.ts
+++ src/types/index.ts
@@ -64,64 +64,98 @@ export type SpotifyPlaylist = {
 };
 
 export type SpotifyTrack = {
-    album: {
-        album_type: string,
-        artists: {
+
+    href: string,
+    limit: number,
+    next: string | null,
+    offset: number,
+    previous: string | null,
+    total: number,
+    items: {
+        added_at: string,
+        added_by: {
             external_urls: {
                 spotify: string
             },
             href: string,
             id: string,
-            name: string,
-            type: string,
+            type: "user",
             uri: string
-        }[],
-        external_urls: {
-            spotify: string
         },
-        href: string,
-        id: string,
-        images: {
-            url: string,
-            height: number,
-            width: number
-        }[],
-        name: string,
-        release_date: string,
-        release_date_precision: string,
-        total_tracks: number,
-        type: string,
-        uri: string
-    },
-    artists: {
-        external_urls: {
-            spotify: string
-        },
-        href: string,
-        id: string,
-        name: string,
-        type: string,
-        uri: string
-    }[],
-    disc_number: number,
-    duration_ms: number,
-    explicit: boolean,
-    external_ids: {
-        isrc: string
-    },
-    external_urls: {
-        spotify: string
-    },
-    href: string,
-    id: string,
-    is_local: boolean,
-    name: string,
-    popularity: number,
-    preview_url: string | null,
-    track_number: number,
-    type: string,
-    uri: string
-};
+        is_local: boolean,
+        track: {
+            album: {
+                album_type: "compilation" | "album" | "single",
+                total_tracks: number,
+                available_markets: string[],
+                external_urls: {
+                    spotify: string
+                },
+                href: string,
+                id: string,
+                images: {
+                    url: string,
+                    height: number,
+                    width: number
+                }[],
+                name: string,
+                release_date: string,
+                release_date_precision: "year" | "month" | "day",
+                restrictions?: {
+                    reason: "market"
+                },
+                type: "album",
+                uri: string,
+                artists: {
+                    external_urls: {
+                        spotify: string
+                    },
+                    href: string,
+                    id: string,
+                    name: string,
+                    type: "artist",
+                    uri: string
+                }[]
+            },
+            artists: {
+                external_urls: {
+                    spotify: string
+                },
+                href: string,
+                id: string,
+                name: string,
+                type: "artist",
+                uri: string
+            }[],
+            available_markets: string[],
+            disc_number: number,
+            duration_ms: number,
+            explicit: boolean,
+            external_ids: {
+                isrc: string,
+                ean: string,
+                upc: string
+            },
+            external_urls: {
+                spotify: string
+            },
+            href: string,
+            id: string,
+            is_playable: boolean,
+            linked_from: {},
+            restrictions?: {
+                reason: string
+            },
+            name: string,
+            popularity: number,
+            preview_url: string,
+            track_number: number,
+            type: "track",
+            uri: string,
+            is_local: boolean
+        }
+    }[]
+}
 
 export type SpotifyTrackItem = {
     added_at: string,
