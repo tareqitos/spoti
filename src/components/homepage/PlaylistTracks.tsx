0f0b72d84324b70590a91e295e806e689c63b414
Tareqitos <contact@tareqitos.com>
2025-05-22 15:11:13 +0200

    Moved playlists tracks generation to its own component for better clarity

--- src/components/homepage/PlaylistTracks.tsx
+++ src/components/homepage/PlaylistTracks.tsx
@@ -1,13 +1,13 @@
-import { useEffect, useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
-import { ArrowDown, ArrowUp, Funnel } from "lucide-react";
+import { useState } from "react";
+import { ArrowDown, ArrowUp } from "lucide-react";
 import { SpotifyTrack, SpotifyTrackItem } from "../../types";
-import { convertDateToLong, convertDuration, sortAsc, sortDesc } from "../helpers/helpers";
-import { useGetPlaylistsQuery, useGetPlaylistTracksQuery } from "../../api/apiSlice";
+import { sortAsc, sortDesc } from "../helpers/helpers";
+import { useGetPlaylistTracksQuery } from "../../api/apiSlice";
+import { Tracks } from "./Tracks";
 
 interface TracksProps {
     tracks: SpotifyTrack,
-    showTrack: (track: SpotifyTrackItem) => void
+    showTrackBar: (track: SpotifyTrackItem) => void
 }
 
 interface SortState {
@@ -15,7 +15,7 @@ interface SortState {
     isAscending: boolean,
 }
 
-export const PlaylistTracks = ({ tracks, showTrack }: TracksProps) => {
+export const PlaylistTracks = ({ tracks, showTrackBar }: TracksProps) => {
     const [items, setItems] = useState(tracks.items);
     const [sortState, setSortState] = useState<SortState>({ activeColumn: null, isAscending: false });
     const [pages, setPages] = useState({ previous: tracks.previous || null, next: tracks.next || null })
@@ -115,52 +115,7 @@ export const PlaylistTracks = ({ tracks, showTrack }: TracksProps) => {
             {
                 items.length !== undefined ?
                     items.map(item => (
-                        <div key={uuidv4()} className="tracks-container">
-
-                            <div className="track-infos">
-                                <img
-                                    src={
-                                        item.track.album?.images[2]?.url ||
-                                        item.track.album?.images[1]?.url ||
-                                        item.track.album?.images[0]?.url ||
-                                        ""}
-                                    alt={item.track.name}
-                                    className="track-image-bg"
-                                />
-
-                                <div className="track-image-container">
-                                    <img
-                                        src={
-                                            item.track.album?.images[2]?.url ||
-                                            item.track.album?.images[1]?.url ||
-                                            item.track.album?.images[0]?.url ||
-                                            "./vinyl.png"}
-                                        alt={item.track.name}
-                                        className="track-image"
-                                    />
-
-
-                                </div>
-                                <div className="track-details">
-                                    <a onClick={() => showTrack(item.track)}>{item.track.name}</a>
-                                    {item.track.album.artists.map(artist => (
-                                        <p key={uuidv4()}>{artist.name}</p>
-                                    ))}
-                                </div>
-                            </div>
-
-                            <div className="track-album">
-                                <p>{item.track.album.name}</p>
-                            </div>
-
-                            <div className="track-release-date">
-                                <p>{convertDateToLong(item.track.album.release_date)}</p>
-                            </div>
-
-                            <div className="track-duration">
-                                <p>{convertDuration(item.track.duration_ms)}</p>
-                            </div>
-                        </div>
+                        <Tracks key={item.track.id} item={item.track} showTrackBar={showTrackBar} />
                     ))
                     :
                     <p>Nothing here</p>
